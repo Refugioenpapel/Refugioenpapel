ccdfe12ad253e7583707e8fdc3c797adb26065fb
Refugioenpapel <mirefugioenpapel@gmail.com>
2025-05-07 19:49:52 -0300

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -5,3 +5,12 @@ const nextConfig: NextConfig = {
 };
 
 export default nextConfig;
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+};
+
+module.exports = nextConfig;
